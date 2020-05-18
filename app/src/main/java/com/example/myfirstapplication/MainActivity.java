5b8d3681f779cbad1ce422cb1e113adb815b292e
liyuanz <13419866507@163.com>
2020-05-18 18:17:05 +0800

    update

---
+++ app/src/main/java/com/example/myfirstapplication/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.myfirstapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.hardware.display.DisplayManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+
+public class MainActivity extends AppCompatActivity {
+public static final String EXTRA_MESSAGE = "com.example.myfirstapplication.MESSAGE";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    /** Called when the user tags the Send button */
+    public void sendMessage(View view){
+        //Do something in response to button
+        //System.out.println("点击了发送消息");
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE,message);
+        startActivity(intent);
+        //Intent intent = new Intent(this, DisplayManagerAct.class);
+    }
+}
