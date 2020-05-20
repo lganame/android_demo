a5ed697b067405217d65e45f61daee7c242ea615
liyuanz <13419866507@163.com>
2020-05-20 16:34:54 +0800

    教程不仔细,我这里综合了线性布局和表格布局,呈单列显示,提交一下,实际我想让按钮呈一排显示

--- app/src/main/java/com/example/myfirstapplication/TableLayout.java
+++ app/src/main/java/com/example/myfirstapplication/TableLayout.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class TableLayout extends AppCompatActivity {
     protected  void  onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tablelayout);
     }
 }
