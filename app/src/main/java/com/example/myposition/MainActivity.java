44b4f4c0f14125b102d400e40ded6e687ba78cdb
liyuanz <13419866507@163.com>
2020-05-21 12:06:51 +0800

    update

--- app/src/main/java/com/example/myposition/MainActivity.java
+++ app/src/main/java/com/example/myposition/MainActivity.java
@@ -2,19 +2,32 @@ package com.example.myposition;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.hardware.display.DisplayManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.myfirstapplication.R;
 
 import java.util.Date;
+import java.util.List;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    //定位都要通过LocationManager这个类实现
+    private LocationManager locationManager;
+    private String provider;
+
+
+
 public static final String EXTRA_MESSAGE = "com.example.myfirstapplication.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +35,71 @@ public static final String EXTRA_MESSAGE = "com.example.myfirstapplication.MESSA
         setContentView(R.layout.activity_main);
     }
 
+    //关闭时解除监听器
+    @Override
+    protected void onDestroy() {
+        // TODO Auto-generated method stub
+        super.onDestroy();
+        if (locationManager != null) {
+            locationManager.removeUpdates(locationListener);
+        }
+    }
+
+
     public void startposition(View view){
         System.out.println(new Date().toString());
         System.out.println("Log.DEBUG"+Log.DEBUG);
+            //获取定位服务
+            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            //获取当前可用的位置控制器
+            List<String> list = locationManager.getProviders(true);
 
+            if (list.contains(LocationManager.GPS_PROVIDER)) {
+                //是否为GPS位置控制器
+                provider = LocationManager.GPS_PROVIDER;
+            }
+            else if (list.contains(LocationManager.NETWORK_PROVIDER)) {
+                //是否为网络位置控制器
+                provider = LocationManager.NETWORK_PROVIDER;
+            } else {
+                Toast.makeText(this, "请检查网络或GPS是否打开", Toast.LENGTH_LONG).show();
+                return;
+            }
+        Location location = locationManager.getLastKnownLocation(provider);
+            if (location != null) {
+                //获取当前位置，这里只用到了经纬度
+                String string = provider.toString()+"纬度为：" + location.getLatitude() + ",经度为：" + location.getLongitude();
+                Toast.makeText(this,string,Toast.LENGTH_LONG).show();
+            }
+
+         //绑定定位事件，监听位置是否改变
+        //第一个参数为控制器类型第二个参数为监听位置变化的时间间隔（单位：毫秒）
+         //第三个参数为位置变化的间隔（单位：米）第四个参数为位置监听器
+            locationManager.requestLocationUpdates(provider, 2000, 2,locationListener);
+        }
+
+
+        LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void onProviderEnabled(String arg0) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void onProviderDisabled(String arg0) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void onLocationChanged(Location arg0) {
+                // TODO Auto-generated method stub
+                // 更新当前经纬度
+            }
+        };
     }
 
-}
